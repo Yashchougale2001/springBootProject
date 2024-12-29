2dec49200089db486144773a57a1cb8ac9858b49
krushnal patil <krushnal5212@gmail.com>
2024-12-29 10:06:46 +0530

    project setup(starting point)

---
+++ src/test/java/in/org/smartcontactmanager/smartcontactmanager/SmartcontactmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.org.smartcontactmanager.smartcontactmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcontactmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
